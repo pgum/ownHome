55222286064dd4009c72a4901f169c0237f4fab3
P <killme@dietpi.(none)>
2015-04-28 23:56:53 +0100

    vundleproblem

--- deployEnv.sh
+++ deployEnv.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 configsNamePattern=ownHome/[B,V,b,v]*rc
-echo $configsNamePattern
-mkdir -p ~/.$configsNamePattern
 mkdir ~/.Bashrc ~/.Vimrc
+
 ln -s -t ~/.Bashrc/ ~/ownHome/Bashrc/*
 ln -s -t ~/.Vimrc/ ~/ownHome/Vimrc/*
 ln -s ~/ownHome/vimrc ~/.vimrc
 ln -s ~/ownHome/bashrc ~/.bashrc
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c"PluginInstall" -c"qa"
