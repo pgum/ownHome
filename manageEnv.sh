9dbcba2355b13980a263c0623e73ae7ad8569572
P <killme@dietpi.(none)>
2015-04-29 01:28:44 +0100

    better listing

--- manageEnv.sh
+++ manageEnv.sh
@@ -2,15 +2,18 @@
 source ~/.bashrc -silent
 sourceBashrc ui.framework
 
-for f in ownHome/Bashrc/[0-9]*
+echo "Bashrc plug-in list:"
+
+for f in ~/ownHome/Bashrc/[0-9]*
 do
   fname=$(basename $f)
-  [ -f ~/.Bashrc/$fname ] && echoOk "[ON ][Std] $fname" || echoError "[OFF][Std] $fname"
+  [ -f ~/.Bashrc/$fname ] && echoOk "[ON ][Std]${stop_color} $fname" || echoError "[OFF][Std]${stop_color} $fname"
 done
 
 for f in ~/.Bashrc/[0-9]*
 do
   fname=$(basename $f)
-  [ "$(readlink $f)" = "$HOME/ownHome/Bashrc/$fname" ] || echoWarn "[ON ][---]$fname"
+  [ "$(readlink $f)" = "$HOME/ownHome/Bashrc/$fname" ] || echoWarn "[ON ][---]${stop_color} $fname"
 done
+
 
