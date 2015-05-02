d0bf9d83bdd7aa313ffdd6f7b9a3db8e7b75d766
pgum <pjgumulka@gmail.com>
2015-05-02 13:42:53 +0100

    changes

--- deployEnv.sh
+++ deployEnv.sh
@@ -6,10 +6,13 @@ showInstructionsFor What-will-be-configured
 showInstructionsFor Config-files-file-structure
 generateConfigsLinks
 
-showInstructionsFor SSH-key-generation-linux-side
-sshKeyGenerationProcedure
+if confirm "Do you want to generate SSH Keys? [y/N]"
+  then
+  showInstructionsFor SSH-key-generation-linux-side
+  sshKeyGenerationProcedure
 
-showInstructionsFor How-to-import-created-SSH-keys-into-PuTTY
+  showInstructionsFor How-to-import-created-SSH-keys-into-PuTTY
+fi
 
 showInstructionsFor Configuration-of-Vim-editor
 installVundleVimPluginManager
