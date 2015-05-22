79d6ba4d0c16a3bc8f6057046597f8bb941112e7
PJG <pjgumulka@gmail.com>
2015-05-22 16:59:00 +0200

    Update deployEnv.sh
    show instructions for ssh and vim before question to apply

--- deployEnv.sh
+++ deployEnv.sh
@@ -6,17 +6,15 @@ showInstructionsFor What-will-be-configured
 
 showInstructionsFor Config-files-file-structure
 generateConfigsLinks
-
+showInstructionsFor SSH-key-generation-linux-side
 if confirm "Do you want to generate SSH Keys? [y/N]"
   then
-  showInstructionsFor SSH-key-generation-linux-side
   sshKeyGenerationProcedure
-
   showInstructionsFor How-to-import-created-SSH-keys-into-PuTTY
 fi
+showInstructionsFor Configuration-of-Vim-editor
 if confirm "Do you want to download, install and configure vundle vim plugin manager? [y/N]"
   then
-  showInstructionsFor Configuration-of-Vim-editor
   installVundleVimPluginManager
   updateVundlePluginInformation
 fi
