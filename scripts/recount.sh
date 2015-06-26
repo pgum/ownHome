2f74d1b7abda47299db0c64c0073384557c05e7f
klm <pgum@github.com>
2015-06-26 20:11:39 +0200

    single recount script with usage recount.sh <*.d directory in configs>

---
+++ scripts/recount.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#if no argument given exit with message
+[ -z $1 ] && echo -n "usage: $0 <" && for dir in $(find $(dirname $0)/../config -name *.d); do echo -n " $(basename $dir)"; done && echo ">"&& exit
+
+#rename files prepending them with rising number follwed by dot sign
+index=10
+step=10
+for f in $(dirname $0)/../config/$1/*
+  do
+    mv $f $i.${f#*.}
+    let index=index+step
+  done
+
+
