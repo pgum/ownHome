803b7a7649506dfedbe336cdeb145f8e00e341be
Piotr Jacek Gumulka <pgum@github.com>
2015-07-24 23:49:05 +0100

    still breaking things

--- scripts/recount.sh
+++ scripts/recount.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #if no argument given exit with message
-[ -z $1 ] && echo -n "usage: $0 <" && for dir in $(find $(dirname $0)/../config -name *.d); do echo -n " $(basename $dir)"; done && echo ">"&& exit
+[ -z $1 ] && echo -n "usage: $0 <" && for dir in $(find $(dirname $0)/../dot -name *.d); do echo -n " $(basename $dir)"; done && echo ">"&& exit
 
 #rename files prepending them with rising number follwed by dot sign
 index=10
 step=10
-for f in $(dirname $0)/../config/$1/*
+for f in $(dirname $0)/../dot/$1/*
   do
     mv $f $i.${f#*.}
     let index=index+step
   done
-
 
